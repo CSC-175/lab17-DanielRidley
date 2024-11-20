a41e92c2197faa561a74384e9adaa19a3a412724
Daniel <ridaniel@umich.edu>
2024-11-20 10:22:16 -0500

    Attempt 1

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,15 @@
 // Definition of findHigh function goes here
+using namespace std;
+template<typename A, typename B,typename C, typename D>
+void findHigh(A Data[],B size,C &index,D &intHigh)
+{
+
+    for(int i=0;i<size;i++)
+    {
+            if(Data[i]>intHigh)
+            {
+                intHigh=Data[i];
+                index=i;
+            }
+    }
+}
